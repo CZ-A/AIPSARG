388e2980929278bd14813a2f0a621c9af32b038d
CZ-A <adrianfalsha@gmail.com>
2025-01-06 18:49:41 +0700

    Update main.py
    Perubahan:
    
    Menambahkan try...except blok pada main loop untuk menagkap exception yang mungkin terjadi

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # aipsarg/main.py
 import logging
-from aipsarg.ai.trading_ai import TradingAI
+from aipsarg.bot.trading_ai import TradingAI
 from aipsarg.config.app_config import AppConfig
 from aipsarg.utils.logger import setup_logger
 
@@ -9,9 +9,12 @@ logger = setup_logger(__name__)
 
 
 if __name__ == "__main__":
-    config = AppConfig()
-    if config.validate_config():
-        ai = TradingAI()
-        ai.run()
-    else:
-        logger.error("Invalid configuration, cannot start bot")
+    try:
+        config = AppConfig()
+        if config.validate_config():
+            ai = TradingAI()
+            ai.run()
+        else:
+            logger.error("Invalid configuration, cannot start bot")
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
