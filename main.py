1e39a9dfa9e272de82f02b5de10c76e687228465
CZ-A <adrianfalsha@gmail.com>
2025-01-06 18:13:43 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# aipsarg/main.py
+import logging
+from aipsarg.ai.trading_ai import TradingAI
+from aipsarg.config.app_config import AppConfig
+from aipsarg.utils.logger import setup_logger
+
+# Setup logger
+logger = setup_logger(__name__)
+
+
+if __name__ == "__main__":
+    config = AppConfig()
+    if config.validate_config():
+        ai = TradingAI()
+        ai.run()
+    else:
+        logger.error("Invalid configuration, cannot start bot")
